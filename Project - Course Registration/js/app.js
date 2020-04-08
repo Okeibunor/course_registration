69edd77bdee63152ea09959cc563ef992a8b89f1
Okeibunor Favour <favourokeibunor@gmail.com>
2020-04-08 08:48:31 +0100

    submit button partially working

--- Project - Course Registration/js/app.js
+++ Project - Course Registration/js/app.js
@@ -6,7 +6,7 @@ add_course.addEventListener('click',(event)=>{
   template=`
     <div class="form-row">
     <div class="form-group col-10">
-    <select name="courses" id="courses" class="form-control">
+    <select name="courses" id="courses" class="form-control courses">
       <option value="null">--Select Course--</option>
       <option value="mathematics">Mathematics</option>
       <option value="english">English</option>
@@ -35,4 +35,21 @@ document.addEventListener('click',(event)=>{
 
 document.querySelector("#submit").addEventListener('click',(event)=>{
   event.preventDefault();
+  var student_name = document.getElementById("student_name").value;
+  var student_id = document.getElementById("student_id").value;
+  var courses = document.getElementsByClassName("courses");
+  course_array = [];
+  array = Array.from(courses);
+  array.forEach(element => {
+    course_array.push(element.value);
+  });
+  table_template = `
+    <tr>
+      <td>${student_name}</td>
+      <td>${student_id}</td>
+      <td>${course_array}</td>
+      <td>${new Date()}</td>
+    </tr>
+  `;
+  document.getElementById("container").insertAdjacentHTML("beforeend",table_template);
 });
