bd43dd28dca920bbb98b36d364002ce204065988
Okeibunor Favour <favourokeibunor@gmail.com>
2020-04-09 07:33:20 +0100

    add basic routing

--- Node_projects/meadowlark/site/meadowlark.js
+++ Node_projects/meadowlark/site/meadowlark.js
@@ -4,18 +4,30 @@ app.set('port', process.env.PORT || 3000);
 
 // custom 404 page
 app.use(function(req, res){
-res.type('text/plain');
-res.status(404);
-res.send('404 - Not Found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
 // custom 500 page
 app.use(function(err, req, res, next){
-console.error(err.stack);
-res.type('text/plain');
-res.status(500);
-res.send('500 - Server Error');
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
 });
+
 app.listen(app.get('port'), function(){
 console.log( 'Express started on http://localhost:' +
 app.get('port') + '; press Ctrl-C to terminate.' );
+});
+
+//basic routing 
+app.get('/',(req,res)=>{
+  res.type('text/plain');
+  res.send('Homepage running...');
+});
+
+app.get('/about',(req,res)=>{
+  res.type('text/plain');
+  res.send('About page running...');
 });
